d21b4c3c75bcc4de6a019cf62c7920fd8d56b955
Theodoreg10 <theodoregnimavo6@gmail.com>
2024-03-29 14:30:27 +0100

    pages initialisation

--- cashapp/main/urls.py
+++ cashapp/main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.handle_home_page, name="Home"),
-    path("Home", views.handle_home_page, name="Home"),
+    path("", views.handle_home_page, name="home"),
+    path("home", views.handle_home_page, name="home"),
     ]
--- cashapp/main/views.py
+++ cashapp/main/views.py
@@ -5,3 +5,27 @@ from django.shortcuts import render
 
 def handle_home_page(request):
     return render(request, "home.html")
+
+
+def handle_login_page(request):
+    return render(request, "login.html")
+
+
+def handle_transaction_page(request):
+    return render(request, "transaction.html")
+
+
+def handle_budget_page(request):
+    return render(request, "budget.html")
+
+
+def handle_account_page(request):
+    return render(request, "account.html")
+
+
+def handle_category_page(request):
+    return render(request, "category.html")
+
+
+def handle_report_page(request):
+    return render(request, "report.html")
